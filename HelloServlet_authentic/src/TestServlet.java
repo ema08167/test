805d55bbddff7e2794666a8d233e42b5721b98c1
ema08167 <levoilefloral08740816@gmail.com>
2018-02-05 16:50:17 +0900

    Feb.5th

--- HelloServlet_authentic/src/TestServlet.java
+++ HelloServlet_authentic/src/TestServlet.java
@@ -21,7 +21,7 @@ public class TestServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html: charset=UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
 
 		String username=request.getParameter("username");
 		String password=request.getParameter("password");
